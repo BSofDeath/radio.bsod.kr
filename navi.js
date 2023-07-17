e06eb0d2c12d05d465d246996714a2385fe1a950
Taewoo Kim <ktw5780264@outlook.kr>
2023-07-17 21:00:45 +0900

    알파 0.4. 전국 라디오 스트림에 대응.

--- navi.js
+++ navi.js
@@ -1,22 +1,11 @@
-const seoulBtn = document.querySelector('#seoul-btn');
-const seoulList = document.querySelector('#seoul-list');
-const daejeonBtn = document.querySelector('#daejeon-btn');
-const daejeonList = document.querySelector('#daejeon-list');
-
-function showList(btn, list) {
-    btn.classList.add('activeBtn');
-    list.classList.add('activeList');
-    list.classList.remove('hiddenList');
+const showTable = (city) => {
+    const table = document.getElementsByTagName('table');
+    for (let i = 0; i < table.length; i++) {
+        table[i].classList.add('hidden');
+        if (table[i].classList.contains('visible')) {
+            table[i].classList.remove('visible');
+        }
+    }
+    const target = document.getElementById(city);
+    target.classList.replace('hidden', 'visible');
 }
-function HideList(btn, list) {
-    btn.classList.remove('activeBtn');
-    list.classList.remove('activeList');
-    list.classList.add('hiddenList');
-}
-
-seoulBtn.addEventListener('click', function() {
-    showList(seoulBtn, seoulList); HideList(daejeonBtn, daejeonList);
-});
-daejeonBtn.addEventListener('click', function() {
-    showList(daejeonBtn, daejeonList); HideList(seoulBtn, seoulList);
-});
