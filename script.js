846ba1b81f50ecc5b48773eaabeb4a138dd61b98
Taewoo Kim <ktw5780264@outlook.kr>
2024-09-10 21:34:18 +0900

    body: +1

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 function changeTable() {
-    const selector = document.getElementById('selector');
-    const table = document.getElementsByTagName('table');
-    
+    const selector = document.getElementById("city-select");
+    const table = document.getElementsByTagName("table");
+
     for (let i = 0; i < table.length; i++) {
-        if (table[i].id === selector.value)
-            table[i].classList.add('visible');
-        else
-            table[i].classList.remove('visible');
+        if (table[i].id === selector.value) table[i].classList.add("visible");
+        else table[i].classList.remove("visible");
     }
     getPlaylistLink();
     getBatchLink();
-    
-    localStorage.setItem('selectedValue', selector.value);
+
+    localStorage.setItem("selectedValue", selector.value);
 }
 
 function getPlaylistLink() {
-    const selector = document.getElementById('selector');
-    const a = document.getElementById('playlist_link');
+    const selector = document.getElementById("selector");
+    const a = document.getElementById("playlist_link");
     let result = "https://radio.bsod.kr/playlist/" + selector.value + ".m3u";
     a.href = result;
 }
 
 function getBatchLink() {
-    const selector = document.getElementById('selector');
-    const a = document.getElementById('batch_link');
-    let result = "https://radio.bsod.kr/vlc_batch/vlc-stream_" + selector.value + ".bat";
+    const selector = document.getElementById("selector");
+    const a = document.getElementById("batch_link");
+    let result =
+        "https://radio.bsod.kr/vlc_batch/vlc-stream_" + selector.value + ".bat";
     a.href = result;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var selector = document.getElementById('selector');
-    var savedValue = localStorage.getItem('selectedValue');
-    if (savedValue) { selector.value = savedValue; }
+document.addEventListener("DOMContentLoaded", function () {
+    var selector = document.getElementById("selector");
+    var savedValue = localStorage.getItem("selectedValue");
+    if (savedValue) {
+        selector.value = savedValue;
+    }
     changeTable();
 });
