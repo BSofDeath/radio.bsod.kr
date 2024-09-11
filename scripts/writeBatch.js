a80fd4995cf455b6a78b08eb058a84afab1f3f83
Taewoo Kim <ktw5780264@outlook.kr>
2024-09-12 00:01:30 +0900

    writeBatch.js 불필요 구문 제거

--- scripts/writeBatch.js
+++ scripts/writeBatch.js
@@ -2,7 +2,6 @@ document.getElementById("batch-button").addEventListener("click", function () {
     const selectedCity = document.getElementById("city-select");
     const batContent =
         `@echo off
-cd "C:\Program Files\VideoLAN\VLC"
 start vlc --sout #transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:8080/} --sout-all --sout-keep "https://radio.bsod.kr/playlist/` +
         selectedCity.value +
         `.m3u"`;
