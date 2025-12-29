cb9caa1a09553bd3f3e00317fbe1a07c86715af7
Taewoo Kim <ktw5780264@outlook.kr>
2025-12-29 14:31:23 +0900

    Arirang Radio, 부산영어방송, 글로벌광주방송 채널 추가

--- functions/stream/url/common.js
+++ functions/stream/url/common.js
@@ -4,4 +4,7 @@ export const commonUrls = [
     { name: "iFM 경인방송", station: "ifm", streamUrl: "https://stream.ifm.kr/live/aod1/playlist.m3u8" },
     { name: "OBS 라디오", station: "obs", streamUrl: "https://vod3.obs.co.kr:444/live/obsstream1/radio.stream/playlist.m3u8" },
     { name: "국방FM", station: "kookbang", streamUrl: "https://mediaworks.dema.mil.kr/live_edge/audio.sdp/playlist.m3u8" },
+    { name: "Arirang Radio", station: "arirang", streamUrl: "https://amdlive-ch03-ctnd-com.akamaized.net/arirang_3ch/smil:arirang_3ch.smil/playlist.m3u8"},
+    { name: "부산영어방송", station: "befm", streamUrl: "https://live.cdn.smilecdn.com/befm905_live/live/playlist.m3u8"},
+    { name: "글로벌광주방송", station: "ggn", streamUrl: "http://media.ggn.or.kr:1935/gfnlive/myStream/playlist.m3u8" },
 ];
