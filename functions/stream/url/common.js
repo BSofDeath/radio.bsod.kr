7b2a5b93a7137afac2011e0f72b10ed4071f9bbd
Taewoo Kim <ktw5780264@outlook.kr>
2026-01-01 13:21:33 +0900

    common.js: iFM 경인방송 보이는라디오 대응

--- functions/stream/url/common.js
+++ functions/stream/url/common.js
@@ -1,7 +1,13 @@
 export const commonUrls = [
     { name: "EBS FM", station: "ebs", streamUrl: "https://ebsonair.ebs.co.kr/fmradiofamilypc/familypc1m/playlist.m3u8" },
     { name: "YTN 라디오", station: "ytn", streamUrl: "https://radiolive.ytn.co.kr/radio/_definst_/20211118_fmlive/playlist.m3u8" },
-    { name: "iFM 경인방송", station: "ifm", streamUrl: "https://stream.ifm.kr/live/aod1/playlist.m3u8" },
+    { name: "iFM 경인방송", station: "ifm", streamUrl: async (bora = null) => {
+    try {
+      const url = bora == "true" 
+      ? "https://stream.ifm.kr/live/bora1/playlist.m3u8"
+      : "https://stream.ifm.kr/live/aod1/playlist.m3u8";
+      return url;
+    } catch (e) { return null; }} },  // default
     { name: "OBS 라디오", station: "obs", streamUrl: "https://vod3.obs.co.kr:444/live/obsstream1/radio.stream/playlist.m3u8" },
     { name: "국방FM", station: "kookbang", streamUrl: "https://mediaworks.dema.mil.kr/live_edge/audio.sdp/playlist.m3u8" },
     { name: "Arirang Radio", station: "arirang", streamUrl: "https://amdlive-ch03-ctnd-com.akamaized.net/arirang_3ch/smil:arirang_3ch.smil/playlist.m3u8"},
