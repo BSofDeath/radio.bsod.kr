2773bda168b2d8de89ed942a24a0688989e526c3
HongKee Moon <hkmoon@me.com>
2026-04-25 11:25:45 +0200

    obs, bbs 라디오 링크 수정 #3

--- functions/stream/url/bbs.js
+++ functions/stream/url/bbs.js
@@ -1,6 +1,6 @@
 export const bbsUrls = [
     /* BBS 불교방송 */
-    { name: "BBS 서울불교방송", city: null, streamUrl: "https://bbslive.clouducs.com/bbsradio-live/livestream/playlist.m3u8" },  // default
+    { name: "BBS 서울불교방송", city: null, streamUrl: "https://bbslive.clouducs.com/bbsradio-mlive/radio.stream/playlist.m3u8" },  // default
     { name: "BBS 부산불교방송", city: "busan", streamUrl: "http://live.cdn.smilecdn.com:1935/bbsfm_live/live.sdp/playlist.m3u8" },
     { name: "BBS 대구불교방송", city: "daegu", streamUrl: "https://bbslive.goldenday.kr:446/hls/dgbbs.m3u8" },
     { name: "BBS 광주불교방송", city: "gwangju", streamUrl: "http://live.cdn.smilecdn.com:1935/kjbbs1_live/live/playlist.m3u8" },
--- functions/stream/url/common.js
+++ functions/stream/url/common.js
@@ -8,7 +8,7 @@ export const commonUrls = [
       : "https://stream.ifm.kr/live/aod1/playlist.m3u8";
       return url;
     } catch (e) { return null; }} },  // default
-    { name: "OBS 라디오", station: "obs", streamUrl: "https://vod3.obs.co.kr:444/live/obsstream1/radio.stream/playlist.m3u8" },
+    { name: "OBS 라디오", station: "obs", streamUrl: "https://vod.obs.co.kr:444/live/obsstream1/radio.stream/playlist.m3u8" },
     { name: "국방FM", station: "kookbang", streamUrl: "https://mediaworks.dema.mil.kr/live_edge/audio.sdp/playlist.m3u8" },
     { name: "Arirang Radio", station: "arirang", streamUrl: "https://amdlive-ch03-ctnd-com.akamaized.net/arirang_3ch/smil:arirang_3ch.smil/playlist.m3u8"},
     { name: "부산영어방송", station: "befm", streamUrl: "https://live.cdn.smilecdn.com/befm905_live/live/playlist.m3u8"},
