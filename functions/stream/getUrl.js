d3a424954e86e8dd2b5fb2157a266f7421ac2999
Taewoo Kim <ktw5780264@outlook.kr>
2026-06-01 23:28:03 +0900

    getUrl.js: ebsUrls import 누락 해결

--- functions/stream/getUrl.js
+++ functions/stream/getUrl.js
@@ -1,6 +1,7 @@
 import { kbsUrls } from "./url/kbs.js";
 import { mbcUrls } from "./url/mbc.js";
 import { sbsUrls } from "./url/sbs.js";
+import { ebsUrls } from "./url/ebs.js";
 import { tbsUrls } from "./url/tbs.js";
 import { tbnUrls } from "./url/tbn.js";
 import { cbsUrls } from "./url/cbs.js";
