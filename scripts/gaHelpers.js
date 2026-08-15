c510c77bbfe5271c4f44d3c4937d2fa8378e3901
Taewoo Kim <ktw5780264@outlook.kr>
2026-08-15 23:46:07 +0900

    Added code to handle adblockers in gaHelpers.js

--- scripts/gaHelpers.js
+++ scripts/gaHelpers.js
@@ -1,19 +1,31 @@
 const GA_MEASUREMENT_ID = "G-E26S19D0QV";
- 
+const GA_IDS_TIMEOUT_MS = 800;
+
 function getGaIds() {
     return new Promise((resolve) => {
         if (typeof gtag !== "function") {
             resolve({ cid: null, sid: null });
             return;
         }
+
+        let settled = false;
+        const finish = (result) => {
+            if (settled) return;
+            settled = true;
+            resolve(result);
+        };
+        
         const cidPromise = new Promise((res) =>
             gtag("get", GA_MEASUREMENT_ID, "client_id", res)
         );
         const sidPromise = new Promise((res) =>
             gtag("get", GA_MEASUREMENT_ID, "session_id", res)
         );
-        Promise.all([cidPromise, sidPromise]).then(([cid, sid]) =>
-            resolve({ cid: cid || null, sid: sid || null })
-        );
+
+        Promise.all([cidPromise, sidPromise])
+            .then(([cid, sid]) => finish({ cid: cid || null, sid: sid || null }))
+            .catch(() => finish({ cid: null, sid: null }));
+
+        setTimeout(() => finish({ cid: null, sid: null }), GA_IDS_TIMEOUT_MS);
     });
 }
