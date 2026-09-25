0f1d7eefd99452d7d0335b3ff5feee3f597656db
Taewoo Kim <ktw5780264@outlook.kr>
2026-09-25 23:05:14 +0900

    fix: domain change for tbnUrls

--- functions/stream/url/tbn.js
+++ functions/stream/url/tbn.js
@@ -1,15 +1,15 @@
 export const tbnUrls = [
-    { name: "TBN 경인교통방송", city: null, streamUrl: "https://5cc6beb04faf6.streamlock.net/gyeongin/myStream/playlist.m3u8" },  // default
-    { name: "TBN 부산교통방송", city: "busan", streamUrl: "https://5cc6beb04faf6.streamlock.net/busan/myStream/playlist.m3u8" },
-    { name: "TBN 울산교통방송", city: "ulsan", streamUrl: "https://5cc6beb04faf6.streamlock.net/ulsan/myStream/playlist.m3u8" },
-    { name: "TBN 경남교통방송", city: "gyeongnam", streamUrl: "https://5cc6beb04faf6.streamlock.net/gyeongnam/myStream/playlist.m3u8" },
-    { name: "TBN 대구교통방송", city: "daegu", streamUrl: "https://5cc6beb04faf6.streamlock.net/daegu/myStream/playlist.m3u8" },
-    { name: "TBN 경북교통방송", city: "gyeongbuk", streamUrl: "https://5cc6beb04faf6.streamlock.net/kyungbuk/myStream/playlist.m3u8" },
-    { name: "TBN 광주교통방송", city: "gwangju", streamUrl: "https://5cc6beb04faf6.streamlock.net/gwangju/myStream/playlist.m3u8" },
-    { name: "TBN 전북교통방송", city: "jeonbuk", streamUrl: "https://5cc6beb04faf6.streamlock.net/jeonbuk/myStream/playlist.m3u8" },
-    { name: "TBN 대전교통방송", city: "daejeon", streamUrl: "https://5cc6beb04faf6.streamlock.net/daejeon/myStream/playlist.m3u8" },
-    { name: "TBN 충북교통방송", city: "chungbuk", streamUrl: "https://5cc6beb04faf6.streamlock.net/chungbuk/myStream/playlist.m3u8" },
-    { name: "TBN 충남교통방송", city: "chungnam", streamUrl: "https://5cc6beb04faf6.streamlock.net/chungnam/myStream/playlist.m3u8" },
-    { name: "TBN 강원교통방송", city: "gangwon", streamUrl: "https://5cc6beb04faf6.streamlock.net/gangwon/myStream/playlist.m3u8" },
-    { name: "TBN 경인교통방송", city: "jeju", streamUrl: "https://5cc6beb04faf6.streamlock.net/jeju/myStream/playlist.m3u8" },
+    { name: "TBN 경인교통방송", city: null, streamUrl: "https://www.tbn.it.kr/gyeongin/myStream/playlist.m3u8" },  // default
+    { name: "TBN 부산교통방송", city: "busan", streamUrl: "https://www.tbn.it.kr/busan/myStream/playlist.m3u8" },
+    { name: "TBN 울산교통방송", city: "ulsan", streamUrl: "https://www.tbn.it.kr/ulsan/myStream/playlist.m3u8" },
+    { name: "TBN 경남교통방송", city: "gyeongnam", streamUrl: "https://www.tbn.it.kr/gyeongnam/myStream/playlist.m3u8" },
+    { name: "TBN 대구교통방송", city: "daegu", streamUrl: "https://www.tbn.it.kr/daegu/myStream/playlist.m3u8" },
+    { name: "TBN 경북교통방송", city: "gyeongbuk", streamUrl: "https://www.tbn.it.kr/kyungbuk/myStream/playlist.m3u8" },
+    { name: "TBN 광주교통방송", city: "gwangju", streamUrl: "https://www.tbn.it.kr/gwangju/myStream/playlist.m3u8" },
+    { name: "TBN 전북교통방송", city: "jeonbuk", streamUrl: "https://www.tbn.it.kr/jeonbuk/myStream/playlist.m3u8" },
+    { name: "TBN 대전교통방송", city: "daejeon", streamUrl: "https://www.tbn.it.kr/daejeon/myStream/playlist.m3u8" },
+    { name: "TBN 충북교통방송", city: "chungbuk", streamUrl: "https://www.tbn.it.kr/chungbuk/myStream/playlist.m3u8" },
+    { name: "TBN 충남교통방송", city: "chungnam", streamUrl: "https://www.tbn.it.kr/chungnam/myStream/playlist.m3u8" },
+    { name: "TBN 강원교통방송", city: "gangwon", streamUrl: "https://www.tbn.it.kr/gangwon/myStream/playlist.m3u8" },
+    { name: "TBN 경인교통방송", city: "jeju", streamUrl: "https://www.tbn.it.kr/jeju/myStream/playlist.m3u8" },
 ];
